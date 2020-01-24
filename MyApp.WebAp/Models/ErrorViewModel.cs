8267c8c265be95476c862dd4e43280f1c9ce9dc4
Kapil Chadha <kapil.chadha@avanade.com>
2020-01-24 17:53:20 +0000

    initial commit

---
+++ MyApp.WebAp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyApp.WebAp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
